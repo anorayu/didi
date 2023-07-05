5c3aee24301cb0f9f0b88235e0eb2a1495a22356
Назокат Юлдашева <@corp.itstep>
2023-07-05 20:53:33 +0500

    TASK-2

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print ("hello")
-print("Ali Malikov", "six of january" 1999, 123456, "Samarkand", "Uzbekistan", "street A", "app", 6)
+# print ("hello")
+# print("Ali Malikov", "six of january" 1999, 123456, "Samarkand", "Uzbekistan", "street A", "app", 6)
+class City:
+     def __init__(self, city, district, state, citizens):
+           self.city = city
+           self.district = district
+           self.state = state
+           self.citizens = citizens
+
+
+
+cityFirst = City("Fergana", "Fergana valley", "Uzbekistan", "25")
+
+print(cityFirst.city, cityFirst.district, cityFirst.state, cityFirst.citizens)
+
